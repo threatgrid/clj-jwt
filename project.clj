d72551f1b95e8a4b6f8b8034ffd35d396439a502
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2018-02-28 13:06:18 +0100

    changed homepage

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject yogsototh/clj-jwt "0.2.1"
   :description  "Clojure library for JSON Web Token(JWT)"
-  :url          "https://github.com/liquidz/clj-jwt"
+  :url          "https://github.com/yogsototh/clj-jwt"
   :license      {:name "Eclipse Public License"
                  :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
