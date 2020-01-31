fa77010c8ab39930a89fef3f5ebb5d52832b6530
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2020-01-31 16:47:01 +0100

    Version 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yogsototh/clj-jwt "0.3.0-SNAPSHOT"
+(defproject yogsototh/clj-jwt "0.3.0"
   :description  "Clojure library for JSON Web Token(JWT)"
   :url          "https://github.com/yogsototh/clj-jwt"
   :license      {:name "Eclipse Public License"
