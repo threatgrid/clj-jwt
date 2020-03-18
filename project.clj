fdac01d4c9bea71feec67722ee679be45a21508d
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2020-03-18 12:50:42 -0400

    update coordinate & bump deps

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
-(defproject yogsototh/clj-jwt "0.3.1-SNAPSHOT"
+(defproject threatgrid/clj-jwt "0.3.1-SNAPSHOT"
   :description  "Clojure library for JSON Web Token(JWT)"
-  :url          "https://github.com/yogsototh/clj-jwt"
+  :url          "https://github.com/threatgrid/clj-jwt"
+  :pedantic?    :abort
   :license      {:name "Eclipse Public License"
                  :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/data.json "0.2.6"]
-                 [org.clojure/data.codec "0.1.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/data.json "1.0.0"]
+                 [org.clojure/data.codec "0.1.1"]
                  [org.bouncycastle/bcpkix-jdk15on "1.52"]
                  [crypto-equality "1.0.0"]
-                 [clj-time "0.11.0"]]
-  :profiles {:dev {:dependencies [[midje "1.7.0"  :exclusions [org.clojure/clojure]]]}}
-  :plugins  [[lein-midje "3.1.3"]])
+                 [clj-time "0.15.2"]]
+  :profiles {:dev {:dependencies [[midje "1.9.9"]]}}
+  :plugins  [[lein-midje "3.2.1"]])
