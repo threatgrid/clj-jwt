3c97a28fefe02f55215f5358751ea3b1a7ff6779
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2021-11-17 17:33:41 +0100

    version bump and docstring

--- src/clj_jwt/bouncy_castle_provider.clj
+++ src/clj_jwt/bouncy_castle_provider.clj
@@ -1,4 +1,6 @@
 (ns clj-jwt.bouncy-castle-provider
+  "Same trick used by pandect;
+  see https://github.com/xsc/pandect/blob/main/src/pandect/utils/bouncy_castle_provider.clj"
   (:import java.security.Security
            org.bouncycastle.jce.provider.BouncyCastleProvider))
 
