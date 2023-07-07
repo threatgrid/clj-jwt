6717f43e7e731be57ea000d59d9369a212a7f265
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2023-07-07 12:21:47 +0200

    Version 0.5.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject threatgrid/clj-jwt "0.5.1"
+(defproject threatgrid/clj-jwt "0.5.2-SNAPSHOT"
   :description  "Clojure library for JSON Web Token(JWT)"
   :url          "https://github.com/threatgrid/clj-jwt"
   :pedantic?    :abort
